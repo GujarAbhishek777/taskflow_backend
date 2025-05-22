40b0a2714331b0f6672caf8058fd9d0573db2285
Abhishek <gujarabhishek777@gmail.com>
2025-05-23 00:41:52 +0530

    added correct path

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ Rails.application.routes.draw do
       post 'add_task', to: 'tasks#add_task'
       get 'tasks_data', to: 'tasks#tasks_data'
 
-      get 'pdf_generator',to:'pdf_generators#pdf_generator'
+      post 'pdf_generator',to:'pdf_generators#pdf_generator'
       
 
 
